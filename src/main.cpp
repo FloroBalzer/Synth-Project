31bda531ae7c0c252e7d1abb675086e1d158c621
ZackReeves <zlr20@ic.ac.uk>
2023-03-08 15:43:46 +0000

    Knobs
    Added Volume control and a class for knobs

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,11 @@
 #include <U8g2lib.h>
 #include <iostream>
 #include <string>
+#include <STM32FreeRTOS.h>
+
+#include "knobs.h"
 
 //Constants
-  const uint32_t interval = 100; //Display update interval
 
 //Pin definitions
   //Row select and enable
@@ -33,12 +35,21 @@
   const int DRST_BIT = 4;
   const int HKOW_BIT = 5;
   const int HKOE_BIT = 6;
+
   //other
   volatile uint32_t currentStepSize;
+  volatile int step;
 
   //Step Size Array
   const uint32_t stepSizes [13] = {51076057, 54113197, 57330935, 60740010, 64351799, 68178356, 72232452, 76527617, 81078186, 85899346, 91007189, 96418756, 0 };
   const std::string notes[13] = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B","No key pressed"};
+  
+  //Key Array
+  SemaphoreHandle_t keyArrayMutex;
+  volatile uint8_t keyArray[7];
+
+  //Knobs
+  Knob Volume(8, 8, 0, 3);
 
 //Display driver object
 U8G2_SSD1305_128X32_NONAME_F_HW_I2C u8g2(U8G2_R0);
@@ -58,8 +69,102 @@ void setOutMuxBit(const uint8_t bitIdx, const bool value) {
 void sampleISR() {
   static uint32_t phaseAcc = 0;
   phaseAcc += currentStepSize;
+
+
   int32_t Vout = (phaseAcc >> 24) - 128;
+
+  Vout = Vout >> (8 - Volume.value);
   analogWrite(OUTR_PIN, Vout + 128);
+}
+
+void setRow(uint8_t rowIdx) {
+  digitalWrite(REN_PIN,LOW);
+  unsigned char RA2 = rowIdx & 0b00000100; //RA2
+  unsigned char RA1 = rowIdx & 0b00000010;//RA1
+  unsigned char RA0 = rowIdx & 0b00000001;//RA2
+  digitalWrite(RA2_PIN, RA2);
+  digitalWrite(RA1_PIN, RA1);
+  digitalWrite(RA0_PIN, RA0);
+  digitalWrite(REN_PIN,HIGH);
+
+}
+
+uint8_t readCols() {
+  int a= digitalRead(C0_PIN);
+  int b = digitalRead(C1_PIN);
+  int c = digitalRead(C2_PIN);
+  int d = digitalRead(C3_PIN);
+  uint8_t colout = (a<<3) | (b<<2) | (c<<1) | (d);
+  return colout;
+}
+
+void scanKeysTask(void * pvParameters) {
+
+  const TickType_t xFrequency = 20/portTICK_PERIOD_MS;
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+
+  uint8_t keys;
+
+  while(1) {
+    vTaskDelayUntil( &xLastWakeTime, xFrequency );
+  
+    __atomic_store_n(&step, 12, __ATOMIC_RELAXED);
+
+    for (uint8_t i = 0; i < 4; i++) {
+      setRow(i);
+      delayMicroseconds(3);
+      keys = readCols();
+      
+      xSemaphoreTake(keyArrayMutex, portMAX_DELAY);
+      keyArray[i] = keys;
+      xSemaphoreGive(keyArrayMutex);
+      
+      if(i < 3) {
+        for (uint8_t j = 0; j < 4; j++) { 
+          if (~keys & (1 << j)) {
+              __atomic_store_n(&step, 3-j +(i*4), __ATOMIC_RELAXED);
+          }
+        }
+      }
+
+      __atomic_store_n(&currentStepSize, stepSizes[step], __ATOMIC_RELAXED);
+    }
+
+    Volume.read_knob();
+
+  }
+}
+
+void displayUpdateTask(void * pvParameters) {
+  const TickType_t xFrequency = 100/portTICK_PERIOD_MS;
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+
+  while(1) {
+    vTaskDelayUntil( &xLastWakeTime, xFrequency );
+
+    //Update display
+    u8g2.clearBuffer();         // clear the internal memory
+    u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
+    
+    xSemaphoreTake(keyArrayMutex, portMAX_DELAY);
+    u8g2.setCursor(2,20);
+    u8g2.print(keyArray[2], HEX);
+    u8g2.print(" ");
+    u8g2.print(keyArray[1], HEX);
+    u8g2.print(" ");
+    u8g2.print(keyArray[0], HEX);
+    u8g2.drawStr(2, 10, notes[step].c_str());
+    u8g2.drawStr(2, 30, "Volume: ");
+    u8g2.setCursor(52,30);
+    u8g2.print(Volume.value);
+    u8g2.sendBuffer();  // transfer internal memory to the display
+    xSemaphoreGive(keyArrayMutex);
+
+
+    //Toggle LED
+    digitalToggle(LED_BUILTIN);
+  }
+
 }
 
 void setup() {
@@ -90,7 +195,6 @@ void setup() {
 
   //Initialise UART
   Serial.begin(9600);
-  Serial.println("Hello Max");
 
   //settimer
   TIM_TypeDef *Instance = TIM1;
@@ -99,73 +203,30 @@ void setup() {
   sampleTimer->attachInterrupt(sampleISR);
   sampleTimer->resume();
 
-}
+  TaskHandle_t scanKeysHandle = NULL;
+  xTaskCreate(
+  scanKeysTask,		/* Function that implements the task */
+  "scanKeys",		/* Text name for the task */
+  64,      		/* Stack size in words, not bytes */
+  NULL,			/* Parameter passed into the task */
+  2,			/* Task priority */
+  &scanKeysHandle );	/* Pointer to store the task handle */
 
-void setRow(uint8_t rowIdx) {
-  digitalWrite(REN_PIN,LOW);
-  unsigned char RA2 = rowIdx & 0b00000100; //RA2
-  unsigned char RA1 = rowIdx & 0b00000010;//RA1
-  unsigned char RA0 = rowIdx & 0b00000001;//RA2
-  digitalWrite(RA2_PIN, RA2);
-  digitalWrite(RA1_PIN, RA1);
-  digitalWrite(RA0_PIN, RA0);
-  digitalWrite(REN_PIN,HIGH);
+  TaskHandle_t displayUpdateHandle = NULL;
+  xTaskCreate(
+  displayUpdateTask,		/* Function that implements the task */
+  "displayUpdate",		/* Text name for the task */
+  256,      		/* Stack size in words, not bytes */
+  NULL,			/* Parameter passed into the task */
+  1,			/* Task priority */
+  &displayUpdateHandle );	/* Pointer to store the task handle */
 
-}
+  keyArrayMutex = xSemaphoreCreateMutex();
 
-uint8_t readCols() {
-  int a= digitalRead(C0_PIN);
-  int b = digitalRead(C1_PIN);
-  int c = digitalRead(C2_PIN);
-  int d = digitalRead(C3_PIN);
-  uint8_t colout = (a<<3) | (b<<2) | (c<<1) | (d);
-  return colout;
+  vTaskStartScheduler();
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  static uint32_t next = millis();
-  int step;
-  uint8_t keyArray[7];
-
-  while (millis() < next);
-
-  next += interval;
-  step = 12;
-
-  //Update display
-  u8g2.clearBuffer();         // clear the internal memory
-  u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
-  //u8g2.drawStr(2,10,"Hello Max");// write something to the internal memory
-
-  for (uint8_t i = 0; i < 3; i++) {
-    setRow(i);
-    delayMicroseconds(3);
-    uint8_t keys = readCols();
-    keyArray[i] = keys;
-  }
-  
-  for (uint8_t i = 0; i < 3; i++) {          
-    for (uint8_t j = 0; j < 4; j++) {   
-      if (~keyArray[i] & (1 << j)) {
-          step = 3-j +(i*4);
-      }
-    }
-
-    currentStepSize = stepSizes[step];
-  }
-  
-  u8g2.setCursor(2,20);
-  u8g2.print(keyArray[2], HEX);
-  u8g2.print(" ");
-  u8g2.print(keyArray[1], HEX);
-  u8g2.print(" ");
-  u8g2.print(keyArray[0], HEX);
-  u8g2.drawStr(2, 10, notes[step].c_str());
-  u8g2.sendBuffer();      // transfer internal memory to the display
-  
-
-  //Toggle LED
-  digitalToggle(LED_BUILTIN);
   
 }
