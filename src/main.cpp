80c8f1a860111a3acee4e519afbcedaf4f89839a
kert114 <kertlaansalu@gmail.com>
2023-03-14 19:16:59 +0000

    intial push for polyphony

--- src/main.cpp
+++ src/main.cpp
@@ -7,169 +7,225 @@
 
 #include "knobs.h"
 
-//Constants
+// Constants
 
-//Pin definitions
-  //Row select and enable
-  const int RA0_PIN = D3;
-  const int RA1_PIN = D6;
-  const int RA2_PIN = D12;
-  const int REN_PIN = A5;
+// Pin definitions
+// Row select and enable
+const int RA0_PIN = D3;
+const int RA1_PIN = D6;
+const int RA2_PIN = D12;
+const int REN_PIN = A5;
 
-  //Matrix input and output
-  const int C0_PIN = A2;
-  const int C1_PIN = D9;
-  const int C2_PIN = A6;
-  const int C3_PIN = D1;
-  const int OUT_PIN = D11;
+// Matrix input and output
+const int C0_PIN = A2;
+const int C1_PIN = D9;
+const int C2_PIN = A6;
+const int C3_PIN = D1;
+const int OUT_PIN = D11;
 
-  //Audio analogue out
-  const int OUTL_PIN = A4;
-  const int OUTR_PIN = A3;
+// Audio analogue out
+const int OUTL_PIN = A4;
+const int OUTR_PIN = A3;
 
-  //Joystick analogue in
-  const int JOYY_PIN = A0;
-  const int JOYX_PIN = A1;
+// Joystick analogue in
+const int JOYY_PIN = A0;
+const int JOYX_PIN = A1;
 
-  //Output multiplexer bits
-  const int DEN_BIT = 3;
-  const int DRST_BIT = 4;
-  const int HKOW_BIT = 5;
-  const int HKOE_BIT = 6;
+// Output multiplexer bits
+const int DEN_BIT = 3;
+const int DRST_BIT = 4;
+const int HKOW_BIT = 5;
+const int HKOE_BIT = 6;
 
-  //other
-  volatile uint32_t currentStepSize;
-  volatile int step;
+// other
+volatile uint32_t currentStepSize[13];
+volatile int step;
 
-  //Step Size Array
-  const uint32_t stepSizes [13] = {51076057, 54113197, 57330935, 60740010, 64351799, 68178356, 72232452, 76527617, 81078186, 85899346, 91007189, 96418756, 0 };
-  const std::string notes[13] = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B","No key pressed"};
-  
-  //Key Array
-  SemaphoreHandle_t keyArrayMutex;
-  volatile uint8_t keyArray[7];
+// Step Size Array
+const uint32_t stepSizes[13] = {51076057, 54113197, 57330935, 60740010, 64351799, 68178356, 72232452, 76527617, 81078186, 85899346, 91007189, 96418756, 0};
+const std::string notes[13] = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "No key pressed"};
 
-  //Knobs
-  Knob Volume(8, 8, 0, 3);
-  Knob Octave(0, 3, 0, 2);
+/// key pressed
+volatile bool keypressed;
+// Key Array
+SemaphoreHandle_t keyArrayMutex;
+volatile uint8_t keyArray[7];
 
-//Display driver object
+// Knobs
+Knob Volume(8, 8, 0, 3);
+Knob Octave(0, 3, 0, 2);
+
+// Display driver object
 U8G2_SSD1305_128X32_NONAME_F_HW_I2C u8g2(U8G2_R0);
 
-//Function to set outputs using key matrix
-void setOutMuxBit(const uint8_t bitIdx, const bool value) {
-      digitalWrite(REN_PIN,LOW);
-      digitalWrite(RA0_PIN, bitIdx & 0x01);
-      digitalWrite(RA1_PIN, bitIdx & 0x02);
-      digitalWrite(RA2_PIN, bitIdx & 0x04);
-      digitalWrite(OUT_PIN,value);
-      digitalWrite(REN_PIN,HIGH);
-      delayMicroseconds(2);
-      digitalWrite(REN_PIN,LOW);
+// // queue
+// QueueHandle_t msgInQ;
+// QueueHandle_t msgOutQ;
+
+// // CAN communication variables
+// uint8_t TX_Message[8] = {0};
+// uint8_t RX_Message[8] = {0};
+
+// global handle for a FreeRTOS mutex
+SemaphoreHandle_t RX_Message_Mutex;
+SemaphoreHandle_t CAN_TX_Semaphore;
+
+// Function to set outputs using key matrix
+void setOutMuxBit(const uint8_t bitIdx, const bool value)
+{
+  digitalWrite(REN_PIN, LOW);
+  digitalWrite(RA0_PIN, bitIdx & 0x01);
+  digitalWrite(RA1_PIN, bitIdx & 0x02);
+  digitalWrite(RA2_PIN, bitIdx & 0x04);
+  digitalWrite(OUT_PIN, value);
+  digitalWrite(REN_PIN, HIGH);
+  delayMicroseconds(2);
+  digitalWrite(REN_PIN, LOW);
 }
 
-void sampleISR() {
-  static uint32_t phaseAcc = 0;
+void sampleISR()
+{
+  int32_t Vout = 0;
+  int n = 12;
+  static int32_t phaseAcc[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  if (keypressed)
+  {
+    for (int i = 0; i < n; i++)
+    {
+      if (currentStepSize[i] != 0)
+      {
+        phaseAcc[i] += currentStepSize[i];
+        Vout += (phaseAcc[i] >> 24) - 128;
+      }
+    }
 
-  phaseAcc += currentStepSize << Octave.value;
-
-  int32_t Vout = (phaseAcc >> 24) - 128;
-
-  Vout = Vout >> (8 - Volume.value);
-  analogWrite(OUTR_PIN, Vout + 128);
+    analogWrite(OUTR_PIN, Vout + 128);
+  }
 }
-
-void setRow(uint8_t rowIdx) {
-  digitalWrite(REN_PIN,LOW);
-  unsigned char RA2 = rowIdx & 0b00000100; //RA2
-  unsigned char RA1 = rowIdx & 0b00000010;//RA1
-  unsigned char RA0 = rowIdx & 0b00000001;//RA2
+void setRow(uint8_t rowIdx)
+{
+  digitalWrite(REN_PIN, LOW);
+  unsigned char RA2 = rowIdx & 0b00000100; // RA2
+  unsigned char RA1 = rowIdx & 0b00000010; // RA1
+  unsigned char RA0 = rowIdx & 0b00000001; // RA2
   digitalWrite(RA2_PIN, RA2);
   digitalWrite(RA1_PIN, RA1);
   digitalWrite(RA0_PIN, RA0);
-  digitalWrite(REN_PIN,HIGH);
-
+  digitalWrite(REN_PIN, HIGH);
 }
 
-uint8_t readCols() {
-  int a= digitalRead(C0_PIN);
+uint8_t readCols()
+{
+  int a = digitalRead(C0_PIN);
   int b = digitalRead(C1_PIN);
   int c = digitalRead(C2_PIN);
   int d = digitalRead(C3_PIN);
-  uint8_t colout = (a<<3) | (b<<2) | (c<<1) | (d);
+  uint8_t colout = (a << 3) | (b << 2) | (c << 1) | (d);
   return colout;
 }
+// void generateMSG(volatile uint8_t *)
+// {
+// }
 
-void scanKeysTask(void * pvParameters) {
+// void CAN_RX_ISR(void)
+// {
+//   uint8_t RX_Message_ISR[8];
+//   uint32_t ID;
+//   CAN_RX(ID, RX_Message_ISR);
+//   xQueueSendFromISR(msgInQ, RX_Message_ISR, NULL);
+// }
 
-  const TickType_t xFrequency = 20/portTICK_PERIOD_MS;
+// void CAN_TX_ISR(void)
+// {
+//   xSemaphoreGiveFromISR(CAN_TX_Semaphore, NULL);
+// }
+
+void scanKeysTask(void *pvParameters)
+{
+
+  const TickType_t xFrequency = 20 / portTICK_PERIOD_MS;
   TickType_t xLastWakeTime = xTaskGetTickCount();
 
   uint8_t keys;
 
-  //CAN BUS
-  uint8_t TX_Message[8] = {0};
+  // CAN BUS
+  // uint8_t TX_Message[8] = {0};
 
-  while(1) {
-    vTaskDelayUntil( &xLastWakeTime, xFrequency );
-  
+  while (1)
+  {
+    vTaskDelayUntil(&xLastWakeTime, xFrequency);
+
     __atomic_store_n(&step, 12, __ATOMIC_RELAXED);
 
-    for (uint8_t i = 0; i < 5; i++) {
+    for (uint8_t i = 0; i < 3; i++)
+    {
       setRow(i);
       delayMicroseconds(3);
       keys = readCols();
-      
+
       xSemaphoreTake(keyArrayMutex, portMAX_DELAY);
       keyArray[i] = keys;
+      // std::cout << keyArray[i] << std::endl;
       xSemaphoreGive(keyArrayMutex);
-      
-      if(i < 3) {
-        for (uint8_t j = 0; j < 4; j++) { 
-          if (~keys & (1 << j)) {
-              __atomic_store_n(&step, 3-j +(i*4), __ATOMIC_RELAXED);
-          }
+
+      for (uint8_t j = 0; j < 4; j++)
+      {
+        if (!((keys >> j) & 1))
+        {
+          __atomic_store_n(&currentStepSize[3 - j + 4 * i], stepSizes[3 - j + 4 * i], __ATOMIC_RELAXED);
+        }
+        else
+        {
+          __atomic_store_n(&currentStepSize[3 - j + 4 * i], 0, __ATOMIC_RELAXED);
         }
       }
-
-      __atomic_store_n(&currentStepSize, stepSizes[step], __ATOMIC_RELAXED);
+      if (keyArray[0] == 0xf && keyArray[1] == 0xf && keyArray[2] == 0xf)
+      {
+        keypressed = false;
+      }
+      else
+      {
+        keypressed = true;
+      }
     }
-
-    Volume.read_knob();
-    Octave.read_knob();
-
-    TX_Message[0] = 'p';
-    TX_Message[1] = Octave.value;
-    TX_Message[2] = step;
-
-    CAN_TX(0x123, TX_Message);
-    
+    /// for each key
   }
+  Volume.read_knob();
+  Octave.read_knob();
+
+  // TX_Message[0] = 'p';
+  // TX_Message[1] = Octave.value;
+  // TX_Message[2] = step;
+
+  // CAN_TX(0x123, TX_Message);
 }
 
-void displayUpdateTask(void * pvParameters) {
-  const TickType_t xFrequency = 100/portTICK_PERIOD_MS;
+void displayUpdateTask(void *pvParameters)
+{
+  const TickType_t xFrequency = 100 / portTICK_PERIOD_MS;
   TickType_t xLastWakeTime = xTaskGetTickCount();
 
-  //CAN Bus
-  uint32_t ID;
-  uint8_t RX_Message[8] = {0};
+  // // CAN Bus
+  // uint32_t ID;
+  // uint8_t RX_Message[8] = {0};
 
-  while(1) {
-    vTaskDelayUntil( &xLastWakeTime, xFrequency );
+  while (1)
+  {
+    vTaskDelayUntil(&xLastWakeTime, xFrequency);
 
-    while (CAN_CheckRXLevel()){
-      CAN_RX(ID, RX_Message);
-    }
+    // while (CAN_CheckRXLevel())
+    // {
+    //   CAN_RX(ID, RX_Message);
+    // }
 
-    //Update display
-    u8g2.clearBuffer();         // clear the internal memory
+    // Update display
+    u8g2.clearBuffer();                 // clear the internal memory
     u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
-    
+
     xSemaphoreTake(keyArrayMutex, portMAX_DELAY);
     u8g2.drawStr(2, 10, notes[step].c_str());
 
-    u8g2.setCursor(2,20);
+    u8g2.setCursor(2, 20);
     u8g2.print(keyArray[2], HEX);
     u8g2.print(" ");
     u8g2.print(keyArray[1], HEX);
@@ -177,30 +233,36 @@ void displayUpdateTask(void * pvParameters) {
     u8g2.print(keyArray[0], HEX);
 
     u8g2.drawStr(2, 30, "V: ");
-    u8g2.setCursor(15,30);
+    u8g2.setCursor(15, 30);
     u8g2.print(Volume.value);
     u8g2.drawStr(25, 30, "O: ");
-    u8g2.setCursor(40,30);
+    u8g2.setCursor(40, 30);
     u8g2.print(Octave.value);
 
-    u8g2.setCursor(66,30);
-    u8g2.print((char) RX_Message[0]);
-    u8g2.print(RX_Message[1]);
-    u8g2.print(RX_Message[2]);
+    // u8g2.setCursor(66, 30);
+    // u8g2.print((char)RX_Message[0]);
+    // u8g2.print(RX_Message[1]);
+    // u8g2.print(RX_Message[2]);
 
-    u8g2.sendBuffer();  // transfer internal memory to the display
+    u8g2.sendBuffer(); // transfer internal memory to the display
     xSemaphoreGive(keyArrayMutex);
 
-
-    //Toggle LED
+    // Toggle LED
     digitalToggle(LED_BUILTIN);
   }
-
 }
 
-void setup() {
+// void decodeTask(void *pvParameters)
+// {
+//   while (1)
+//   {
+//     xQueueReceive(msgInQ, RX_Message, portMAX_DELAY);
+//   }
+// }
+void setup()
+{
 
-  //Set pin directions
+  // Set pin directions
   pinMode(RA0_PIN, OUTPUT);
   pinMode(RA1_PIN, OUTPUT);
   pinMode(RA2_PIN, OUTPUT);
@@ -217,51 +279,69 @@ void setup() {
   pinMode(JOYX_PIN, INPUT);
   pinMode(JOYY_PIN, INPUT);
 
-  //Initialise display
-  setOutMuxBit(DRST_BIT, LOW);  //Assert display logic reset
+  // Initialise display
+  setOutMuxBit(DRST_BIT, LOW); // Assert display logic reset
   delayMicroseconds(2);
-  setOutMuxBit(DRST_BIT, HIGH);  //Release display logic reset
+  setOutMuxBit(DRST_BIT, HIGH); // Release display logic reset
   u8g2.begin();
-  setOutMuxBit(DEN_BIT, HIGH);  //Enable display power supply
+  setOutMuxBit(DEN_BIT, HIGH); // Enable display power supply
 
-  //Initialise UART
+  // Initialise UART
   Serial.begin(9600);
 
-  //settimer
+  // settimer
   TIM_TypeDef *Instance = TIM1;
   HardwareTimer *sampleTimer = new HardwareTimer(Instance);
   sampleTimer->setOverflow(22000, HERTZ_FORMAT);
   sampleTimer->attachInterrupt(sampleISR);
   sampleTimer->resume();
 
-  //Initialise CAN
-  CAN_Init(true);
-  setCANFilter(0x123,0x7ff);
-  CAN_Start();
+  // // Initialise CAN
+  // CAN_Init(true);
+  // setCANFilter(0x123, 0x7ff);
+  // CAN_RegisterRX_ISR(CAN_RX_ISR);
+  // CAN_RegisterTX_ISR(CAN_TX_ISR);
+  // CAN_Start();
+  // CAN_Start();
+
+  // // Initialise queue handler
+  // msgInQ = xQueueCreate(36, 8);
+  // msgOutQ = xQueueCreate(36, 8);
 
   TaskHandle_t scanKeysHandle = NULL;
   xTaskCreate(
-  scanKeysTask,		/* Function that implements the task */
-  "scanKeys",		/* Text name for the task */
-  64,      		/* Stack size in words, not bytes */
-  NULL,			/* Parameter passed into the task */
-  2,			/* Task priority */
-  &scanKeysHandle );	/* Pointer to store the task handle */
+      scanKeysTask,     /* Function that implements the task */
+      "scanKeys",       /* Text name for the task */
+      64,               /* Stack size in words, not bytes */
+      NULL,             /* Parameter passed into the task */
+      2,                /* Task priority */
+      &scanKeysHandle); /* Pointer to store the task handle */
 
   TaskHandle_t displayUpdateHandle = NULL;
   xTaskCreate(
-  displayUpdateTask,		/* Function that implements the task */
-  "displayUpdate",		/* Text name for the task */
-  256,      		/* Stack size in words, not bytes */
-  NULL,			/* Parameter passed into the task */
-  1,			/* Task priority */
-  &displayUpdateHandle );	/* Pointer to store the task handle */
+      displayUpdateTask,     /* Function that implements the task */
+      "displayUpdate",       /* Text name for the task */
+      256,                   /* Stack size in words, not bytes */
+      NULL,                  /* Parameter passed into the task */
+      1,                     /* Task priority */
+      &displayUpdateHandle); /* Pointer to store the task handle */
+
+  // Initialise Decode loop
+  // TaskHandle_t decodeHandle = NULL;
+  // xTaskCreate(
+  //     decodeTask,   /* Function that implements the task */
+  //     "decode",     /* Text name for the task */
+  //     256,          /* Stack size in words, not bytes */
+  //     NULL,         /* Parameter passed into the task */
+  //     1,            /* Task priority */
+  //     &decodeHandle /* Pointer to store the task handle */
+  // );
 
   keyArrayMutex = xSemaphoreCreateMutex();
 
   vTaskStartScheduler();
 }
 
-void loop() {
-  
+void loop()
+{
 }
